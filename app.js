3a59436590af70d594a81477d14ea110288eb4ed
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-02-11 11:37:26 +0800

    update with dummy broadcast function

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ AWS.config.update({ region: 'ap-southeast-2' });
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
+const apiGateway = new AWS.ApiGatewayManagementApi({ endpoint: process.env.WS_ENDPOINT });
+
 /**
- * @type {AWS.DynamoDB.DocumentClient.QueryInput}
+ * @typedef {AWS.DynamoDB.DocumentClient.QueryInput} QueryInput
+ * @type {QueryInput}
  */
 const params = {
   KeyConditionExpression: 'BoardId = :BoardId',
@@ -19,12 +22,44 @@ const params = {
   ProjectionExpression: 'active_ws_connections',
 };
 
-docClient
-  .query(params)
-  .promise()
-  .then((res) => {
-    console.log(JSON.stringify(res, null, 2));
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+/**
+ * @param {QueryInput} params valid aws query input parameters
+ * @param {string} attrName the attribute name for the connectionIds
+ * @returns {string[]} - the connectionIds in the board
+ */
+async function getConnectionIds(params, attrName) {
+  return await docClient
+    .query(params)
+    .promise()
+    .then((res) => {
+      const [items] = res.Items;
+      /**
+       * @type {Set}
+       */
+      const { values: connectionIds } = items[attrName];
+      return connectionIds;
+    })
+    .catch((err) => {
+      console.error(err.message);
+      return [];
+    });
+}
+
+/**
+ * @param {string[]} connectionIds
+ * @param {object} message
+ */
+async function broadMessageToConnections(connectionIds, message) {
+  return await Promise.allSettled(
+    connectionIds.map(async (connectionId) =>
+      apiGateway.postToConnection({ ConnectionId: connectionId, data: message }).promise()
+    )
+  );
+}
+
+(async () => {
+  const connectionIds = await getConnectionIds(params, 'active_ws_connections');
+  console.log({ connectionIds });
+  const res = await broadMessageToConnections(connectionIds, '');
+  console.log(JSON.stringify(res, null, 2));
+})();
